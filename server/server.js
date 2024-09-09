be78e4c3c4df7221a1bd4093b20112ef3d6b8621
huy1235588 <viettien4869@gmail.com>
2024-09-09 20:38:05 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const connectDB = require('./src/config/db');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Kết nối đến MongoDB
+connectDB();
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma
+app.use(cors({
+    origin: 'https://localhost:3000/', // Chỉ cho phép ở cổng 3000
+    methods: ['GET', 'POST', 'DELETE', 'PUT'], // Chỉ cho phép các phương thức HTTP này
+    allowedHeaders: [
+        'Content-Type', // Cho biết định dạng của dữ liệu đang được gửi hoặc nhận
+        'Authorization', // Gửi thông tin xác thực
+        'Cache-Control', // Điều khiển cách thức cache (lưu trữ tạm thời)
+        'Expires', // xác định thời gian mà tài nguyên sẽ hết hạn.
+        'Pragma'
+    ],
+    credentials: true, // Cho phép gửi thông tin xác thực như cookie, token... 
+}))
+
+app.use(cookieParser()); // Phân tích các cookie trong yêu cầu HTTP
+app.use(express.json()); // Phân tích dữ liệu JSON trong body ủa yêu cầu HTTP
+
+const PORT = process.env.PORT || 3001; // Cổng
+
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost:${PORT}`);
+});
