00838f39e16e0ec8418877266b6afaade8c3d0b5
huy1235588 <viettien4869@gmail.com>
2024-09-10 15:21:07 +0700

    indent format code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  server: {
-    port: 3000,
-  }
+    server: {
+        port: 3000,
+    }
 })
