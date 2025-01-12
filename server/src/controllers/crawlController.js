a91bcd96c55e28a830b6700842a7d1362a681b7a
huy1235588 <viettien4869@gmail.com>
2025-01-13 00:21:31 +0700

    feat (server): implement JSON data interaction functions for adding and reading data

--- server/src/controllers/crawlController.js
+++ server/src/controllers/crawlController.js
@@ -1,59 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const { type } = require('os');
-
-// Hàm thêm dữ liệu vào tệp JSON
-function addDataToJson(filePath, newData) {
-    try {
-        // Đọc nội dung của tệp JSON
-        let data = [];
-        if (fs.existsSync(filePath)) {
-            const fileContent = fs.readFileSync(filePath, 'utf8');
-            data = JSON.parse(fileContent); // Chuyển đổi từ chuỗi JSON sang đối tượng
-        }
-
-        // Kiểm tra nếu dữ liệu là mảng
-        if (!Array.isArray(data)) {
-            console.error('Dữ liệu trong tệp không phải là một mảng!');
-            return;
-        }
-
-        // Kiểm tra xem dữ liệu mới có tồn tại trong mảng không
-        const isExist = data.some(item => item.appId === newData.appId);
-        if (isExist) {
-            console.error('Dữ liệu mới đã tồn tại trong mảng!');
-            return;
-        }
-
-        // Thêm đối tượng mới vào mảng
-        data.push(newData);
-
-        // Ghi lại dữ liệu cập nhật vào tệp JSON
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-        console.log('Đã thêm dữ liệu mới vào tệp JSON!');
-    } catch (error) {
-        console.error('Đã xảy ra lỗi:', error);
-    }
-}
-
-// Hàm đọc dữ liệu từ tệp JSON
-function readDataFromJson(filePath) {
-    try {
-        // Đọc nội dung của tệp JSON
-        if (fs.existsSync(filePath
-        )) {
-            const fileContent = fs.readFileSync(filePath, 'utf8');
-            const data = JSON.parse(fileContent); // Chuyển đổi từ chuỗi JSON sang đối tượng
-            return data;
-        } else {
-            console.error('Tệp không tồn tại!');
-            return [];
-        }
-    } catch (error) {
-        console.error('Đã xảy ra lỗi:', error);
-        return [];
-    }
-}
+const { readDataFromJson, addDataToJson } = require('../utils/interactJson');
 
 // Craw dữ liệu từ URL
 const crawlByURL = async (req, res) => {
---
+++ server/src/utils/interactJson.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+// Hàm thêm dữ liệu vào tệp JSON
+function addDataToJson(filePath, newData) {
+    try {
+        // Đọc nội dung của tệp JSON
+        let data = [];
+        if (fs.existsSync(filePath)) {
+            const fileContent = fs.readFileSync(filePath, 'utf8');
+            data = JSON.parse(fileContent); // Chuyển đổi từ chuỗi JSON sang đối tượng
+        }
+
+        // Kiểm tra nếu dữ liệu là mảng
+        if (!Array.isArray(data)) {
+            console.error('Dữ liệu trong tệp không phải là một mảng!');
+            return;
+        }
+
+        // Kiểm tra xem dữ liệu mới có tồn tại trong mảng không
+        const isExist = data.some(item => item.appId === newData.appId);
+        if (isExist) {
+            console.error('Dữ liệu mới đã tồn tại trong mảng!');
+            return;
+        }
+
+        // Thêm đối tượng mới vào mảng
+        data.push(newData);
+
+        // Ghi lại dữ liệu cập nhật vào tệp JSON
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+        console.log('Đã thêm dữ liệu mới vào tệp JSON!');
+    } catch (error) {
+        console.error('Đã xảy ra lỗi:', error);
+    }
+}
+
+// Hàm đọc dữ liệu từ tệp JSON
+function readDataFromJson(filePath) {
+    try {
+        // Đọc nội dung của tệp JSON
+        if (fs.existsSync(filePath
+        )) {
+            const fileContent = fs.readFileSync(filePath, 'utf8');
+            const data = JSON.parse(fileContent); // Chuyển đổi từ chuỗi JSON sang đối tượng
+            return data;
+        } else {
+            console.error('Tệp không tồn tại!');
+            return [];
+        }
+    } catch (error) {
+        console.error('Đã xảy ra lỗi:', error);
+        return [];
+    }
+}
+
+module.exports = {
+    addDataToJson,
+    readDataFromJson,
+};
