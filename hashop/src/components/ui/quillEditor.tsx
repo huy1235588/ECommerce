acf8cba5038f147f82797cdff9780459d57c84df
huy1235588 <viettien4869@gmail.com>
2025-03-07 16:06:20 +0700

    feat(quillEditor): enhance Quill editor with custom image handling and improved text change event management

--- hashop/src/components/ui/quillEditor.tsx
+++ hashop/src/components/ui/quillEditor.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import "@/styles/assets/quill.snow.css";
 import Quill from "quill";
+import Delta from 'quill-delta';
 import { Box, InputLabel } from "@mui/material";
 
 interface QuillEditorProps {
@@ -52,6 +53,7 @@ const toolbarQuill = [
 
 const QuillEditor: React.FC<QuillEditorProps> = ({
     id,
+    name,
     label,
     labelOptional = "",
     placeholder = "",
@@ -63,41 +65,74 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
     errorText
 }) => {
     const quillRef = useRef<HTMLDivElement>(null);
-    const quill = useRef<Quill>();
+    const quill = useRef<Quill | null>(null);
 
     useEffect(() => {
         if (quillRef.current) {
             // Khởi tạo Quill
             quill.current = new Quill(quillRef.current, {
                 theme: 'snow',
-                placeholder: placeholder,
                 bounds: quillRef.current,
-                debug: 'info',
+                placeholder: placeholder,
                 modules: {
-                    toolbar: toolbarQuill
+                    toolbar: {
+                        container: toolbarQuill,
+                        handlers: {
+                            customImage: customImage
+                        }
+                    }
                 }
             });
 
-            // Gán giá trị cho Quill
+            // Thiết lập nội dung ban đầu
             if (value) {
-                quill.current.root.innerHTML = value;
+                quill.current.setText(""); // Xóa nội dung mặc định
+                quill.current.clipboard.dangerouslyPasteHTML(value); // Gán nội dung mới
+            }
+
+            // Thêm icon cho customImage
+            const customImageButton = document.querySelector('.ql-customImage');
+            if (customImageButton) {
+                customImageButton.innerHTML = `
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1" 
+                        viewBox="0 0 48 48" enable-background="new 0 0 48 48" height="18px" width="18px" 
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill="#aaa" d="M40,41H8c-2.2,0-4-1.8-4-4V11c0-2.2,1.8-4,4-4h32c2.2,0,4,1.8,4,4v26C44,39.2,42.2,41,40,41z"></path>
+                        <circle fill="#fff" cx="35" cy="16" r="3"></circle>
+                        <polygon fill="#ddd" points="20,16 9,32 31,32"></polygon>
+                        <polygon fill="#fff" points="31,22 23,32 39,32"></polygon>
+                        <circle fill="#43A047" cx="38" cy="38" r="10"></circle>
+                        <g fill="#fff">
+                            <rect x="36" y="32" width="4" height="12"></rect>
+                            <rect x="32" y="36" width="12" height="4"></rect>
+                        </g>
+                    </svg>
+                `;
             }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    // Xử lý khi value thay đổi
+    // Xử lý sự kiện thay đổi văn bản
     useEffect(() => {
-        // Xử lý khi thay đổi nội dung
         if (quill.current) {
-            quill.current.on('text-change', () => {
-                const html = quill.current?.root.innerHTML ?? '';
-                onChange(html);
-                setError(id, '');
-            });
+            // Xử lý khi thay đổi nội dung
+            const handler = (delta: Delta, oldDelta: Delta, source: string) => {
+                if (source === "user") {
+                    const html = quill.current!.root.innerHTML;
+                    onChange(html);
+                    setError(id, "");
+                }
+            };
+
+            quill.current.on("text-change", handler);
+
+            // Cleanup khi component unmount
+            return () => {
+                quill.current!.off("text-change", handler);
+            };
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [value, onChange]);
+    }, [onChange, id, setError]);
 
     // Xử lý khi có lỗi
     useEffect(() => {
@@ -108,8 +143,37 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
         }
     }, [error]);
 
+    // Hàm xử lý custom image
+    const customImage = () => {
+        if (!quill.current) return;
+
+        // @ts-expect-error: tooltip property is missing type definitions from Quill's theme
+        const tooltip = quill.current.theme.tooltip;
+        const originalSave = tooltip.save;
+        const originalHide = tooltip.hide;
+
+        tooltip.save = function () {
+            const range = quill.current?.getSelection(true);
+            const value = tooltip.textbox.value;
+            if (value && range) {
+                quill.current?.insertEmbed(range.index, 'image', value, 'user');
+            }
+        };
+
+        // Gọi hàm save khi nhấn Enter
+        tooltip.hide = function () {
+            tooltip.save = originalSave;
+            tooltip.hide = originalHide;
+            tooltip.hide();
+        };
+
+        tooltip.edit('image');
+        tooltip.textbox.placeholder = 'Image URL';
+    };
+
     return (
         <Box
+            className={name}
             sx={{
                 width: "100%",
                 margin: "1rem 0 0.5rem",
@@ -144,7 +208,9 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
             <div
                 id={id}
                 ref={quillRef}
-                style={{ height: '300px' }}
+                style={{
+                    height: '300px'
+                }}
             ></div>
 
             {error && errorText ? (
